d2fe02f91fa020023428b50363d2dd3ee7f05ae2
bcarlee <ilnrzakirov@gmail.om>
2022-07-27 09:39:41 +0300

    tset

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -2,13 +2,9 @@
 #define STACK_
 #include <deque>
 
-//T - тип элементов хранящихся в стеке
-//С - тип контейнера, использующегося для контролирования последовательности элементов
+
 namespace ft {
-    /*
-     * class T - тип элемента хранящегося в стеке
-     * class C - тип контейнера, использующегося для контролирования последовательности элементов
-     */
+
 template<class T, class C = std::deque<T> >
     class stack {
 public:
@@ -16,29 +12,29 @@ public:
    typedef typename C::value_type value_type;
    typedef typename C::size_type size_type;
 
-        explicit stack(const C& Cont) : c(Cont) { } //определяет исходную последовательность, копии Cont
-        stack() : c() { } //определяет пустую исходную последовательность
+        explicit stack(const C& Cont) : c(Cont) { }
+        stack() : c() { }
         bool empty() const
             {return (c.empty()); }
         size_type size() const
             {return (c.size()); }
-        value_type& top() //возвращает ссылку на последний элемент последовательности (изменяймый)
+        value_type& top()
             {return (c.back()); }
         const value_type& top() const
             {return  (c.back()); }
-        void push(const value_type& X) //вставляет элемент в конец последовательности
+        void push(const value_type& X)
             {c.push_back(X); }
-        void pop() //удаляет последний элемент последовательностий
+        void pop()
             {c.pop_back(); }
-        bool Eq(const stack<T, C>& X) const //минимизирует доступ через шаблонные функции
+        bool Eq(const stack<T, C>& X) const
             {return (c == X.c); }
-        bool Lt(const stack<T, C>& X) const //минимизирует доступ через шаблонные функции
+        bool Lt(const stack<T, C>& X) const
             {return (c < X.c); }
 protected:
     C c;
     };
 
-    /* Шаблонные функции stack */
+
 template<class T, class C> inline
     bool operator==(const stack<T, C>& X, const stack<T, C>& Y)
         {return (X.Eq((Y))); }
@@ -57,5 +53,5 @@ template<class T, class C> inline
 template<class T, class C> inline
     bool operator>=(const stack<T, C>& X, const stack<T, C>& Y)
     {return (!(X < Y)); }
-} /* namespace ft */
-#endif /* STACK_ */
+}
+#endif
